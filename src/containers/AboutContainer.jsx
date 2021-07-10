3ad05d3efdf0b0a30bcaa860fafa92d5c3cabdbb
Mitesh Jethmalani <mitesh.jethmalani@gmail.com>
2021-07-10 20:14:51 +0530

    UI rectifications

--- src/containers/AboutContainer.jsx
+++ src/containers/AboutContainer.jsx
@@ -20,6 +20,8 @@ const About = (props) => {
                 '-webkit-transform': 'translate(-50%, -50%)',
                 transform: 'translate(-50%, -50%)',
             }
+        }, logo: {
+            maxWidth: '100%'
         }
     }))
     const classes = useStyles();
@@ -30,7 +32,7 @@ const About = (props) => {
                     <Container align="center">
                         <img src={AppLogo} alt="app-logo" className={classes.logo} />
                     </Container>
-                    <Typography align="center" color="primary" variant="h2" gutterBottom>About Delta Store</Typography>
+                    <Typography align="center" color="primary" variant="h2" gutterBottom>About us</Typography>
                     <Typography align="center" color="secondary" variant="h6" gutterBottom>
                         We are small but motivated company specializing in New Arrivals. We believe passionately in great bargains and excellent service, which is why we commit ourselves to giving you the best of both.
                         If you’re looking for something new, you’re in the right place. We strive to be industrious and innovative, offering our customers something they want, putting their desires at the top of our priority list.
